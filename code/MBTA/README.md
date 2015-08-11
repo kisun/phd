ee858aea81c25a1ba2f6e49cb2e5b5ca6636b0d8
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-08-11 13:58:56 +1200

    track a vehicle using GTFS Realtime + (R)ProtoBuf

---
+++ code/MBTA/README.md
@@ -0,0 +1,9 @@
+MBTA GTFS
+=========
+
+To download the GTFS static (schedule) data, run
+```
+./setup.sh
+```
+
+For now, the processing is done with `R` because it's most familiar, and easy to quickly implement things.
---
+++ code/MBTA/main.R
@@ -0,0 +1,84 @@
+## Dependencies and settings:
+require("RProtoBuf")
+readProtoFiles("gtfs-realtime.proto")
+url <- "http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb"
+
+trips <- read.csv("gtfs/trips.txt", header = TRUE, stringsAsFactors = FALSE)
+shapes <- read.csv("gtfs/shapes.txt", header = TRUE, stringsAsFactors = FALSE)
+stops <- read.csv("gtfs/stops.txt", header = TRUE, stringsAsFactors = FALSE)
+stop.times <- read.csv("gtfs/stop_times.txt", header = TRUE, stringsAsFactors = FALSE)
+
+
+getVehicles <- function() {
+  system(sprintf("curl -s -o vehiclepositions.pb %s", url))
+  positions <- read(transit_realtime.FeedMessage, "vehiclepositions.pb")
+  activeIDs <- sapply(positions$entity, function(vh) vh$vehicle$vehicle$id)
+
+  list(positions = positions, activeIDs = activeIDs)
+}
+
+trackVehicle <- function(id, proto = getVehicles()) {
+  activeIDs <- proto$activeIDs
+  positions <- proto$positions
+
+  if (id %in% activeIDs) {
+    track <- positions$entity[[which(activeIDs == id)]]$vehicle
+    tripID <- track$trip$trip_id
+
+    shapeID <- trips$shape_id[trips$trip_id == tripID]
+    shape <- shapes[shapes$shape_id == shapeID, ]
+
+    stopIDs <- stop.times$stop_id[stop.times$trip_id == tripID]
+    stop <- stops[stops$stop_id %in% stopIDs, ]
+
+    pos <- c(track$position$longitude, track$position$latitude)
+
+    with(shape, plot(shape_pt_lon, shape_pt_lat, type = "l",
+                     xlim = range(shape_pt_lon, pos[1]),
+                     ylim = range(shape_pt_lat, pos[2]),
+                     main = ID, asp = 1))
+    with(stop, points(stop_lon, stop_lat, pch = 19))
+    with(shape, points(shape_pt_lon[1], shape_pt_lat[1],
+                       pch = 15, col = "green4"))
+    with(shape, points(shape_pt_lon[nrow(shape)], shape_pt_lat[nrow(shape)],
+                       pch = 15, col = "red"))
+    points(pos[1], pos[2], col = "blue", pch = 19)
+
+    return(track)
+  } else {
+    return("That route is no longer available.")
+  }
+}
+printGTFS <- function(x) {
+  writeLines(as.character(x))
+}
+
+followVehicle <- function(id) {
+  proto = getVehicles()
+
+  while (id %in% proto$activeIDs) {
+    printGTFS(trackVehicle(id, proto = proto))
+    Sys.sleep(20)
+    proto <- getVehicles()
+  }
+
+  cat("That route is no longer available ...\n")
+  return(proto$activeIDs)
+}
+
+## First things first - find out what vehicles are currently in service:
+latest <- getVehicles()
+
+## Have a look at a single vehicle
+trackVehicle("v0703")
+
+## All vehicles sequentially
+for (ID in activeIDs) {
+  trackVehicle(ID)
+  Sys.sleep(2)
+}
+
+
+## Follow a vehicle
+getVehicles()$activeIDs
+res <- followVehicle("v2196")
