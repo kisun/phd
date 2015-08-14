5f315b28ac7615d2ac2540a5eacf2e99c756d6f0
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-08-14 13:11:48 +1200

    tracking stuff

--- code/MBTA/track.R
+++ code/MBTA/track.R
@@ -1,3 +1,4 @@
+require(RSQLite)
 require(lubridate)
 
 list.vehicles <- function() {
@@ -18,18 +19,23 @@ track <- function(x) {
     if (class(x) == "track") {
         ## Vehicle identified, N
         N <- track$N
+        AVL <- AVLreport(N)
+        z <- NA
     } else if (missing(x)) {
         stop("Provide either a track, or vehicle ID.")
+    } else {
+        N <- x
+    
+    
+        ## Last associated AVL report
+        AVL <- AVLreport(N)
+        
+        ## Trip, T
+        T <- AVL$B
+
+        ## Distance into trip, z
+        z <- NA
     }
-    
-    ## Last associated AVL report
-    AVL <- AVLreport(N)
-    
-    ## Trip, T
-    T <- AVL$B
-    
-    ## Distance into trip, z
-    z <- NA
     
     ## TPI
     TPI <- NA
@@ -78,7 +84,7 @@ plot.track <- function(x, ...) {
     plot(shape, type = "l", asp = 1)
     points(schedule$lon, schedule$lat, pch = 19, cex = 0.5)
     points(shape[1, , drop = FALSE], col = "black", pch = 19)
-    points(x$AVL["lon"], x$AVL["lat"], pch = 19, col = "red")
+    points(x$AVL["lon"], x$AVL["lat"], pch = 19, col = rgb(1,0, 0, 0.4))
 }
 
 faesible <- function(T, t) {
@@ -86,14 +92,90 @@ faesible <- function(T, t) {
     ## - 20 mins AHEAD of t, or 90 min behind
 
     sched <- schedule(T)
-    time <- hms(format(as.POSIXct(t), "%H:%M:%S"))
-
+    time <- hms(format(as.POSIXct(t, origin = "1970-01-01", tz = "EDT5EDT"), "%H:%M:%S"))
     arrivals <- hms(sched$arrive)
 
-    arrivals - time
+    diff <- arrivals - time
+    which(diff < minutes(20) & diff > minutes(-90))
 }
 
-cur <- track(N)
+
+whereIsTheBus <- function(x, bus = TRUE) {
+    source("functions.R")
+    ## Where on the trip/route is the bus??
+
+    if (bus) {
+        rll <- x$AVL[c("lon", "lat")]
+    } else {
+        rll <- schedule(x)[, c("lon", "lat")]
+    }
+
+
+    apply(rll, 1, function(rl) {
+        r <- as.numeric(convertGPS(rl["lat"], rl["lon"]))
+        sh <- shape(x)
+        
+        p <- convertGPS(sh$lat, sh$lon)
+        d <- c(0, sapply(2:nrow(p), function(i) pathDistance(sh$lat[1:i], sh$lon[1:i])))
+        
+        D <- numeric(nrow(p) - 1)
+        R <- D
+        for (k in 1:(nrow(p) - 1)) {
+            q1 <- as.numeric(p[k, ])
+            q2 <- as.numeric(p[k + 1, ])
+            
+            if (all(q1 == q2)) D[k] <- R[k] <- NA
+            
+            ##        plot(c(r[1], q1[1], q2[1]), c(r[2], q1[2], q2[2]), pch = 19)
+            ##        lines(rbind(q1, q2))
+            
+            v <- q2 - q1
+            w <- r - q1
+            ww <- w %*% w
+            wv <- w %*% v
+            vv <- v %*% v
+            if (dp < 0) {
+                r2 <- ww
+                D[k] <- 0
+                R[k] <- sqrt(r2)
+            } else if (0 <= wv && wv <= vv) {
+                d2 <- wv^2 / vv
+                r2 <- ww - d2
+                R[k] <- sqrt(r2)
+                D[k] <- sqrt(d2)
+            } else {
+                r2 <- (w - v) %*% (w - v)
+                d2 <- vv
+                R[k] <- sqrt(r2)
+                D[k] <- sqrt(d2)
+            }
+        }
+        kk <- which.min(R)
+        
+        dist <- d[kk] + D[kk]
+        
+        dist
+    })
+}
+
+
+list.vehicles()
+cur <- track("v1450")
 plot(cur)
 
 schedule(cur)
+
+
+T <- cur$T; t <- cur$AVL$t
+schedule(cur)[faesible(T, t), ]
+
+
+layout(matrix(c(1, 1, 1, 2)))
+plot(cur)
+busPos <- whereIsTheBus(cur)
+stopPos <- whereIsTheBus(cur, bus = FALSE)
+
+plot(stopPos, rep(1, length(stopPos)), pch = 19, cex = 0.5)
+lines(stopPos[c(1, length(stopPos))], c(1, 1))
+points(busPos, 1, pch = 19, col = rgb(1, 0, 0, 0.4))
+
