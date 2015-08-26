eb6619de838ebfe19bd6955da12923fba83ef922
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-08-26 14:28:02 +1200

    tracker nwo working with KF smoother

--- code/MBTA/tracker.R
+++ code/MBTA/tracker.R
@@ -20,7 +20,7 @@ query <- function(sql, ..., alt.con = NULL) {
     
     if (inherits(q, "try-error")) {
         print(XX)
-        stop("The above query failed.")
+        stop("The above query failed:\n", q)
     }
     
     q
@@ -56,12 +56,12 @@ pathDistance <- function(lat, lon) {
 getAVL <- function(vehicle.id, timestamp = NULL) {
     ## if timestamp is set, obtain AVL from historical data; otherwise from latest
     if (is.null(timestamp)) {
-        AVL <- query("SELECT vehicle_id, trip_id, timestamp, position_latitude AS lat, position_longitude AS lon FROM vehicle_positions WHERE vehicle_id = '%s'", alt.con = dbConnect(SQLite(), "gtfs.db"), vehicle.id)
+        AVL <- query("SELECT vehicle_id, trip_id, timestamp, position_latitude AS lat, position_longitude AS lon FROM vehicle_positions WHERE vehicle_id = '%s' LIMIT 1", alt.con = dbConnect(SQLite(), "gtfs.db"), vehicle.id)
         
         if (nrow(AVL) == 0)
             stop("That vehicle isn't running. Maybe you want to specify a timestamp and use historical data?")
     } else {
-        AVL <- query("SELECT vehicle_id, trip_id, timestamp, position_latitude AS lat, position_longitude AS lon FROM vehicle_positions WHERE vehicle_id='%s' AND timestamp='%s'", vehicle.id, timestamp)
+        AVL <- query("SELECT vehicle_id, trip_id, timestamp, position_latitude AS lat, position_longitude AS lon FROM vehicle_positions WHERE vehicle_id='%s' AND timestamp='%s' LIMIT 1", vehicle.id, timestamp, alt.con = dbConnect(SQLite(), HISTDB))
 
         if (nrow(AVL) == 0)
             stop("No results found. Check the vehicle.id and timestamp are valid.")
@@ -186,14 +186,29 @@ create.gtfs.avl<- function(x, ...) {
                 valid <- which(distToStop < sigma & goForward)
 
             i <- 1
-            while (length(valid) == 1) {
+            while (length(valid) == 1 |
+                       (length(unique(shape[valid, "shape_pt_lat"])) == 1 &
+                            length(unique(shape[valid, "shape_pt_lon"])) == 1)) {
                 valid <- which(distToStop < i * sigma & goForward)
                 i <- i + 1
             }
-                
-            
+
             closest <- closestPoint(stop.pos, shape[valid, ])
 
+            if (length(closest$dist) == 0) {
+                cat("stop.pos:\n")
+                print(stop.pos)
+
+                cat("\nvalid:\n")
+                print(valid)
+
+                cat("\nshape[valid, ]:\n")
+                print(shape[valid, ])
+
+                cat("\nclosest:\n")
+                print(closest)
+            }
+        
             stops$shape_dist_traveled[w] <- closest$dist
 
             if (w == 1)
@@ -298,7 +313,8 @@ propose <- function(track, AVL) {
     
 
 
-    layout(matrix(c(1, 1, 1, 2), ncol = 1))
+    layout(matrix(c(1, 1, 1, 2, 3, 3, 3), ncol = 1))
+    par(mar = c(3.1, 4.1, 1.1, 1.1))
 
     plot(SHAPE$shape_pt_lon, SHAPE$shape_pt_lat, type = "l", asp = 1,
          xlim = range(c(SHAPE$shape_pt_lon, AVL$pos[2])),
@@ -368,9 +384,8 @@ kalmanFilter <- function(new, old) {
     X <- Xk
     attr(X, "time") <- new$AVL$time
     attr(X, "P") <- Pk
-    attr(X, "history") <- rbind(attr(old, "history"), drop(X))
-
-    print(X)
+    attr(X, "history") <- rbind(tail(attr(old, "history"), 50), c(drop(X), new$AVL$time, new$distance.into.trip))
+    rownames(attr(X, "history")) <- NULL
     X
 }
 
@@ -380,6 +395,8 @@ trackMyBus <- function(vehicle.id, timestamp = NULL, prev = NULL) {
 
     ## ERRORS
     assign("sigma", 100, envir = .GlobalEnv)  # meters
+
+    dev.hold()
     
     require(RSQLite)
     if (!"trackers.db" %in% list.files())
@@ -391,8 +408,14 @@ trackMyBus <- function(vehicle.id, timestamp = NULL, prev = NULL) {
     AVL <- getAVL(vehicle.id, timestamp)
     
     ## Does the vehicle exist in the trackers.db database?
-    check <- query("SELECT vehicle_id FROM tracks WHERE vehicle_id='%s'", vehicle.id)
-    if (nrow(check) == 0) create(AVL)
+    check <- query("SELECT vehicle_id FROM tracks WHERE vehicle_id='%s' AND trip_id='%s'", vehicle.id, AVL$trip.id)
+    if (nrow(check) == 0) {
+        reset <- TRUE
+        query("DELETE FROM tracks WHERE vehicle_id='%s'", vehicle.id)
+        create(AVL)
+    } else {
+        reset <- FALSE
+    }
 
     track <- getTrack(vehicle.id)
 
@@ -402,26 +425,112 @@ trackMyBus <- function(vehicle.id, timestamp = NULL, prev = NULL) {
     newtrack$AVL <- AVL
     
     if (is.null(prev)) {
-        KF <- cbind(c(x = newtrack$distance.into.trip * 3.28084, v = 30, a = 0))
+        KF <- cbind(c(x = newtrack$distance.into.trip * 3.28084, v = 1000, a = 100))
         attr(KF, "time") <- AVL$time
-    } else { 
+    } else if (reset) {
+        KF <- prev
+        KF[1, ] <- 0
+        print(KF)
+        attr(KF, "time") <- AVL$time
+        attr(KF, "history") <- rbind(attr(KF, "history"), NA)
+    } else {
         KF <- kalmanFilter(newtrack, prev)
     }
+
+    if (!is.null(attr(KF, "history"))) {
+        HH <- as.data.frame(attr(KF, "history"))
+        colnames(HH) <- c("x", "v", "a", "time", "dit")
+        HH$x <- HH$x * 0.3048
+        
+
+        qry <- query("SELECT stop_id, arrival_time, departure_time, shape_dist_traveled FROM stop_times WHERE trip_id='%s' ORDER BY stop_sequence", track$trip)
+        qry$timepoint <- ifelse(is.na(qry$arrival_time), qry$departure_time, qry$arrival_time)
+        qry$time <- as.numeric(as.POSIXct(qry$timepoint, format="%H:%M:%S", tz="EST5EDT"))        
+        
+        plot(HH$time, HH$dit,
+             xlim = range(HH$time, qry$time),
+             ylim = range(HH$x, HH$dit, qry$shape_dist_traveled),
+             pch = 19, cex = 0.5)
+        lines(HH$time, HH$x)
+        points(qry$time, qry$shape_dist_traveled, col = "#00000040", pch = 19, cex = 0.5)
+
+        abline(h = qry$shape_dist_traveled, col = "#00000010")
+        
+        print(tail(HH, 5))
+    }
+    dev.flush()
     
     list(track = newtrack,
          kalman.filter = KF)
 }
 
 
-vehicle.id <- "v1205"
+vehicle.id <- "v2181"
 timestamp <- NULL
 
 library(RSQLite)
+HISTDB <- "gtfs.db"
 
-query("SELECT vehicle_id FROM vehicle_positions", alt.con = dbConnect(SQLite(), "gtfs.db"))$vehicle_id
+table(query("SELECT vehicle_id FROM vehicle_positions", alt.con = dbConnect(SQLite(), HISTDB))$vehicle_id)
 
-vid <- "v2244"
-trOld <- trackMyBus(vid)
+vid <- "v0730"
+
+tt <- trackMyBus(vid)
+
+while (TRUE){#diff(tail(attr(tt$kalman.filter, "history")[, 5], 2)) >= 0) {
+    tt <- trackMyBus(vid, prev = tt$kalman.filter)
+    Sys.sleep(10)
+}
+
+
+#HISTDB <- "backups/gtfs-historical.backup-latest.db"
+hist <- query("SELECT DISTINCT vehicle_id, trip_id, timestamp FROM vehicle_positions WHERE vehicle_id='%s'", vid,
+              alt.con = dbConnect(SQLite(), HISTDB))
+sort(table(hist$trip_id))
+
+trip.id <- "27532855"
+hist <- hist[hist$trip_id == trip.id, ]
+hist$date <- as.Date(as.POSIXct(hist$timestamp, origin = "1970-1-1", tz = "EST"))
+dates <- unique(hist$date)
+
+hist0 <- hist[hist$date == dates[1], ]
+
+tt <- hist <- NULL
+for (i in 1:nrow(hist0)) {
+    if (is.null(tt))
+        tt <- trackMyBus(vid, timestamp = hist0$timestamp[i])
+    else
+        tt <- trackMyBus(vid, timestamp = hist0$timestamp[i], prev = tt$kalman.filter);i <- i+1
+
+    hist <- rbind(hist, c(tt$track$AVL$pos, tt$track$AVL$time, tt$track$distance.into.trip, tt$track$TPI,
+                          tt$kalman.filter * 0.3048))
+    #Sys.sleep(2)
+}
+colnames(hist) <- c("lat", "lon", "time", "dist", "TPI", "K.dist", "v", "a")
+hist <- as.data.frame(hist)
+
+plot(hist$time, hist$dist)
+lines(hist$time, hist$K.dist)
+
+plot(hist$time, hist$v, type = "l")
 
 tr <- trackMyBus(vid, prev = trOld$kalman.filter)
 tr <- trackMyBus(vid, prev = tr$kalman.filter)
+
+
+
+
+## PROBLEM:
+## - GTFS-realtime trip_id is not correct - doesn't update automatically?!?!?! 
+
+histcon <- dbConnect(SQLite(), HISTDB)
+q <- query("SELECT v.trip_id, v.vehicle_id, position_latitude, position_longitude, timestamp, t.block_id, t.shape_id FROM vehicle_positions AS v, trips AS t WHERE vehicle_id='%s' AND v.trip_id=t.trip_id", vid, alt.con = histcon)
+
+blocks <- unique(q$block_id)
+
+b1 <- q[q$block_id == blocks[2], ]
+shapes1 <- unique(b1$shape_id)
+shapes <- query("SELECT shape_id, shape_pt_lat, shape_pt_lon FROM shapes WHERE shape_id IN %s", shapes1)
+
+plot(shapes$shape_pt_lon, shapes$shape_pt_lat, cex = 0.5, pch = 19)
+points(b1$position_longitude, b1$position_latitude, cex = 0.5, pch = 19, col = "red")
