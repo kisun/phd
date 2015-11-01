5003834619fee0bcd3bc51a3a0fe02c1b8e7f034
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-11-02 09:22:21 +1300

    remove absolute path

--- code/MBTA/get_gtfsr.sh
+++ code/MBTA/get_gtfsr.sh
@@ -1,5 +1,5 @@
 echo "Saving latest GTFS Realtime results to database `gtfs` ..."
 
-/home/tell029/Documents/uni/gtfsrdb/gtfsrdb.py -p http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb -d sqlite:///gtfs.db -c -o
+~/Documents/uni/gtfsrdb/gtfsrdb.py -p http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb -d sqlite:///gtfs.db -c -o
 
 echo "Done?"
