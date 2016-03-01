33ef6f7bf68b1326f1c551ca08b1a77804b6c140
Tom Elliott <tom.elliott@auckland.ac.nz>
2016-03-01 16:02:47 +1300

    vectorise

--- code/auckland_transport/scripts/getHistory.R
+++ code/auckland_transport/scripts/getHistory.R
@@ -4,13 +4,13 @@ args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) stop("Specify date.")
 
-if (length(args) > 1) warning("Multiple arguments, using the first.")
-
-date <- args[1]
-
 ##### 
 
 setwd("~/Documents/uni/phd/code/auckland_transport")
 invisible(sapply(list.files("src/R", pattern = "R$", all.files = TRUE, full.names = TRUE), source))
 
-collectHistory(day = date)
+lapply(args, function(date) try(collectHistory(day = date)))
+
+
+
+
