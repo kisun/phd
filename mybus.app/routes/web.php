da9511351eeed2d2de62b9758e6a90446adc65c9
Tom Elliott <tomelliottnz@gmail.com>
2016-09-06 15:07:13 +1200

    attmpted to get positions : failed

--- mybus.app/routes/web.php
+++ mybus.app/routes/web.php
@@ -18,3 +18,44 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+
+
+Route::get('/test', function() {
+    require_once 'HTTP/Request2.php';
+
+    $request = new Http_Request2('https://api.at.govt.nz/v2/public/realtime/tripupdates');
+    $url = $request->getUrl();
+
+    $headers = array(
+        // Request headers
+        'Ocp-Apim-Subscription-Key' => env('AT_KEY'),
+        'Accept' => 'application/x-protobuf'
+    );
+
+    $request->setHeader($headers);
+
+    $parameters = array(
+        // Request parameters
+        // 'callback' => 'application/x-protobuf',
+        //'tripid' => '{string}',
+        //'vehicleid' => '{string}',
+    );
+
+    $url->setQueryVariables($parameters);
+
+    $request->setMethod(HTTP_Request2::METHOD_GET);
+
+    // Request body
+    //$request->setBody("{body}");
+
+    try
+    {
+        $response = $request->send();
+        dd($response->getBody());
+    }
+    catch (HttpException $ex)
+    {
+        echo $ex;
+    }
+});
