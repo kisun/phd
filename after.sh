e0a47d4029773d0f8952c8da0fd194f28b1ece64
Tom Elliott <tomelliottnz@gmail.com>
2016-09-07 20:29:01 +1200

    fix provisioning

--- after.sh
+++ after.sh
@@ -5,12 +5,15 @@
 # be run after the Homestead machine is provisioned.
 
 ## Install GTFSDB
-apt-get -y update && apt-get -y install python-psycopg2
-git clone https://github.com/OpenTransitTools/gtfsdb.git
-cd gtfsdb && easy_install zc.buildout && easy_install . && buildout install prod postgresql
+if [ ! -d "/home/vagrant/gtfsdb" ]; then
+  apt-get -y update && apt-get -y install python-psycopg2
+  git clone https://github.com/OpenTransitTools/gtfsdb.git /home/vagrant/gtfsdb
+  cd /home/vagrant/gtfsdb && easy_install zc.buildout && easy_install . && buildout install prod postgresql
+fi
 
 ## Install GTFSRDB
-cd
-pip install --upgrade gtfs-realtime-bindings
-git clone https://github.com/tmelliott/gtfsrdb.git
-ln -fs ~/gtfsrdb/gtfsrdb.py /usr/local/bin/gtfsrdb.py
+if [ ! -d "/home/vagrant/gtfsrdb" ]; then
+  pip install --upgrade gtfs-realtime-bindings
+  git clone https://github.com/tmelliott/gtfsrdb.git /home/vagrant/gtfsrdb
+  ln -fs /home/vagrant/gtfsrdb/gtfsrdb.py /usr/local/bin/gtfsrdb.py
+fi
