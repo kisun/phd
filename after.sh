a88620e71794d3af9eae53903ffc44ef26f1e3ee
Tom Elliott <tomelliottnz@gmail.com>
2016-09-07 17:47:34 +1200

    fix provisioner to include gtfsrdb

--- after.sh
+++ after.sh
@@ -3,7 +3,14 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
-#
-apt-get install python-psycopg2
+
+## Install GTFSDB
+apt-get -y update && apt-get -y install python-psycopg2
 git clone https://github.com/OpenTransitTools/gtfsdb.git
 cd gtfsdb && easy_install zc.buildout && easy_install . && buildout install prod postgresql
+
+## Install GTFSRDB
+cd
+pip install --upgrade gtfs-realtime-bindings
+git clone https://github.com/tmelliott/gtfsrdb.git
+ln -fs ~/gtfsrdb/gtfsrdb.py /usr/local/bin/gtfsrdb.py
