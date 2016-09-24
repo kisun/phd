7a400ec1bdbf6a6c4165bef43d65a351e3615fda
Tom Elliott <tomelliottnz@gmail.com>
2016-09-24 13:31:55 +1200

    kernel density? but it loks wrong

--- mybus.app/resources/views/d3.blade.php
+++ mybus.app/resources/views/d3.blade.php
@@ -2,66 +2,56 @@
 
 @section('content')
   <div class="page page-test">
-    <div id="graph"></div>
     <button class="btn btn-warning">Resample</button>
+    <div id="graph"></div>
   </div>
 @endsection
 
 @section('endmatter')
   <script src="https://d3js.org/d3.v4.min.js"></script>
   <script>
-    var data = d3.range(100).map(d3.randomBates(10));
+    var width = 500,
+        height = 300,
+        N = 10000;
 
-    // var formatCount = d3.format(",.0f");
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g");
 
-    var margin = {top: 10, right: 30, bottom: 30, left: 30},
-        width = 400,
-        height = 200;
+    var data = d3.range(N).map(function() { return d3.randomNormal(0, 2)(); });
 
-    var x = d3.scaleLinear().rangeRound([0, width]);
-    var bins = d3.histogram()
-                 .domain(x.domain())
-                 .thresholds(x.ticks(20))
-                 (data);
+    var x = d3.scaleLinear()
+        .domain([-5, 5])
+        .range([0, width]);
 
     var y = d3.scaleLinear()
-              .domain([0, d3.max(bins, function(d) { return d.length; })])
-              .range([height, 0]);
+        .domain([0, .2])
+        .range([height, 0]);
 
-    var svg = d3.select('#graph').append('svg')
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var line = d3.line()
+        .x(function(d) { return x(d[0]); })
+        .y(function(d) { return y(d[1]); });
 
-    var bar = svg.selectAll(".bar")
-                 .data(bins)
-                 .enter().append("g")
-                   .attr("class", "bar")
-                   .attr("transform", function(d) {
-                     return "translate(" + x(d.x0) + "," + y(d.length) + ")";
-                   });
-    bar.append("rect")
-       .attr("x", 1)
-       .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-       .attr("height", function(d) { return height - y(d.length); });
+    var kde = kernelDensityEstimator(epanechnikovKernel(7), x.ticks(100));
 
-    svg.append("g")
-       .attr("class", "axis axis--x")
-       .attr("transform", "translate(0, " + height + ")")
-       .call(d3.axisBottom(x));
+    svg.append("path")
+        .datum(kde(data))
+        .attr("class", "line")
+        .attr("d", line);
 
-    $(".page-test button").on('click', function() {
-      var newdata = d3.range(100).map(d3.randomBates(10));
-      var newbins = d3.histogram()
-                   .domain(x.domain())
-                   .thresholds(x.ticks(20))
-                   (newdata);
-      var newy = d3.scaleLinear()
-                .domain([0, d3.max(newbins, function(d) { return d.length; })])
-                .range([height, 0]);
-      console.log(bar);
-      bar.data(newdata).attr("height", function(d) {return height - newy(d.length); });
-    });
+    function kernelDensityEstimator(kernel, x) {
+      return function(sample) {
+        return x.map(function(x) {
+          return [x, d3.mean(sample, function(v) { return kernel(x - v); })];
+        });
+      }
+    }
+
+    function epanechnikovKernel(scale) {
+      return function(u) {
+        return Math.abs(u /= scale) <= 1 ? .75 * (1 - u * u) / scale : 0;
+      };
+    }
   </script>
 @endsection
